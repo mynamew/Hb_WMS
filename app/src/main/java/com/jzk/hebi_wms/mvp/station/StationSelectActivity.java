eafeed8a2e698c8a1f0e7c531a62433cc3facd17
jzk <893721871@qq.com>
2018-07-19 17:23:12 +0800

    timi：增加工位选择

--- app/src/main/java/com/jzk/hebi_wms/mvp/station/StationSelectActivity.java
+++ app/src/main/java/com/jzk/hebi_wms/mvp/station/StationSelectActivity.java
@@ -10,7 +10,11 @@ import com.jzk.hebi_wms.data.station.StationRequest;
 import com.jzk.hebi_wms.utils.LogUitls;
 import com.jzk.hebi_wms.utils.SpUtils;
 import com.jzk.hebi_wms.view.MyDialog;
-
+/**
+  * 工位选择界面
+  * author: timi
+  * create at: 2018/7/19 17:21
+  */  
 public class StationSelectActivity extends BaseActivity<StationSelectView, StationSelectPresenter> implements StationSelectView {
 
     StationRequest request = new StationRequest();
@@ -29,7 +33,7 @@ public class StationSelectActivity extends BaseActivity<StationSelectView, Stati
                     .setTextViewContent(R.id.tv_title, "错误信息")
                     .setTextViewContent(R.id.tv_content, "请先选择工序再进行此操作！")
                     .setButtonListener(R.id.btn_cancel, null, dialog -> {
-
+                        onBackPressed();
                     }).show();
         }
         request.setEmployeeCode(processSelectCode);
