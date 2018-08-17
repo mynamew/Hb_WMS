a4385881980ac10eb96084959d7ab873abb9dfa6
jzk <893721871@qq.com>
2018-08-17 11:24:59 +0800

    timi：1、CNC修复Bug

--- app/src/main/java/com/jzk/hebi_wms/http/callback/OnResultCallBack.java
+++ app/src/main/java/com/jzk/hebi_wms/http/callback/OnResultCallBack.java
@@ -2,12 +2,19 @@ package com.jzk.hebi_wms.http.callback;
 
 /**
  * 网络请求的返回
- * author: timi
- * create at: 2017-08-15 11:26
+ * @author: jzk
+ * create at: 2017-08-15 11:26     
  */
 public interface OnResultCallBack<T> {
-    //成功的返回
+    /**
+     * 成功的返回
+     * @param t
+     */
     void onSuccess(T t);
-    //失败的返回
+
+    /**
+     * 失败的返回
+     * @param errorMsg
+     */
     void onError(String errorMsg);
 }
